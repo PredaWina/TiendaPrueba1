eee3d36413d12a23ade407ba0017ec90582f997b
PredaWina <pablogscraft@gmail.com>
2020-09-16 16:34:26 +0200

    Primer commit

---
+++ scripts/script.js
@@ -0,0 +1,46 @@
+var numero_carrito = 0;
+var cont = 0;
+
+function sumar_carrito(){
+
+    numero_carrito = numero_carrito + 1;
+    console.log(numero_carrito);
+    document.getElementById('numero_de_elementos').innerHTML = numero_carrito;
+    if (numero_carrito == 10){
+        var obj = document.getElementById("numero_de_elementos");
+        obj.style.marginRight =  '-40%';
+        var obj1 = document.getElementById("carrito_imagen_id");
+        obj1.style.paddingLeft =  '20%'
+    }
+} 
+
+function canviar_modo(){ //  canviar modo claro/oscuro
+    console.log("canviando de modo...");
+
+    x = document.getElementById('cabecera');
+    x.style.borderColor = (x.style.borderColor == 'rgb(26, 26, 26)') ? 'white' : 'rgb(26, 26, 26)';
+
+    y = document.getElementById('cuerpo');
+    y.style.borderColor = (y.style.borderColor == 'rgb(26, 26, 26)') ? 'white' : 'rgb(26, 26, 26)';
+
+    z = document.getElementById('anuncios');
+    z.style.borderColor = (z.style.borderColor == 'rgb(26, 26, 26)') ? 'white' : 'rgb(26, 26, 26)';
+
+    w = document.getElementById('body');
+    w.style.backgroundColor = (w.style.backgroundColor == 'rgb(26, 26, 26)') ? 'white' : 'rgb(26, 26, 26)'; 
+
+
+    if(cont == 0){      //ns porque la imagen no va con lo anterior asi que lo he hecho con if
+        v = document.getElementById('imagen_bombilla');
+        v.src = '/img/bombilla_off.png';
+        cont = 1;
+    }
+    else{
+        document.getElementById("imagen_bombilla");
+        v.src = '/img/bombilla_on.png';
+        cont = 0;
+    }
+   
+}
+    
+
